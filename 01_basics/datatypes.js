bcdf65c6c26789124908f688c24f7b348972d809
Vishaka B R <130122481+wishforvish@users.noreply.github.com>
2024-12-30 14:45:19 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,28 @@
+"use strict";
+ /* treat all JS code as newer version , treats all the older version code as new version, also we don't really have to write this line since JS automatically does its work  */
+
+
+ // alert("hello") 
+ /*here we're using node js not browser so no pop up of alert message */
+
+ //maintain the code readability high and no need to use semicolon ; all the time
+
+let name = "vish" //string datatype ""
+let age = 21 //number datatype (range 2^53 or 52 bits)
+let isLoggedIn = true // boolean datatype 
+let state = null //null -> standalone value, representation of empty value 
+let marks; //undefined -> when value is not defined 
+//symbol -> unique
+//object 
+
+
+
+//two ways we can see the type of a data 
+/*
+1)Directly specifying
+2)Using the variables assigned
+*/
+console.log(typeof "rama")
+console.log(typeof age)
+console.log(typeof null) //o/p: object
+console.log(typeof undefined) // o/p: undefined
