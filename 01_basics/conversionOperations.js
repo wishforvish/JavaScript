7f62c3e49e089cb80a51122b156904515ae114b9
Vishaka B R <130122481+wishforvish@users.noreply.github.com>
2025-01-01 13:57:37 +0000

    Conversion of Operations

--- 01_basics/conversionOperations.js
+++ 01_basics/conversionOperations.js
@@ -28,3 +28,40 @@ let num = 45
 let stringNum = String(num)
 console.log(stringNum) //45
 console.log(typeof stringNum) // string
+
+
+// **************Operations****************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3)
+console.log(2/3)
+console.log(2%3)
+
+let str1 = "hello"
+let str2 = " Vish"
+
+let str3 = str1 + str2 
+console.log(str3);
+
+console.log("1" + 2);  //12
+console.log(1 + "2");  //12
+console.log("1" + 2 + 2); //122
+console.log(1 + 2 + "2"); //32
+
+console.log(true); //true
+console.log(+true); //1
+console.log(+""); //0, not preferred
+
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2  //not ok coz not so readability
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
